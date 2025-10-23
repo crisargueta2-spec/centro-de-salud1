69b9d5746b26cd8b142d1a1d6d805184a141b670
crisargueta2-spec <crisargueta2@mail.com>
2025-10-22 23:49:52 -0600

    Elimino debug y confirmo APP_URL funcional

--- redir_test.php
+++ redir_test.php
@@ -1,7 +1,5 @@
 <?php
 require_once __DIR__ . '/includes/config.php';
-echo "APP_URL: " . APP_URL . "<br>";
-echo "Redirigiendo a: " . APP_URL . "roles/admin_dashboard.php<br>";
 header('Location: ' . APP_URL . 'roles/admin_dashboard.php');
 exit;
 ?>
