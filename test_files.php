c33d6133ab31945dd4acbdb893d7411a3df7d300
crisargueta2-spec <crisargueta2@mail.com>
2025-10-22 23:30:54 -0600

    Agrego test_files.php para verificar estructura en Railway

--- test_files.php
+++ test_files.php
@@ -2,4 +2,11 @@
 echo "<pre>";
 print_r(scandir(__DIR__));
 echo "</pre>";
+
+if (is_dir(__DIR__ . '/roles')) {
+    echo "\n\nContenido de /roles:\n";
+    print_r(scandir(__DIR__ . '/roles'));
+} else {
+    echo "\n\n❌ No existe la carpeta /roles en este deploy.";
+}
 ?>
