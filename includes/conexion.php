4d948a17287b9c712c1b14af20a840e3ae89c3c9
crisargueta2-spec <crisargueta2@mail.com>
2025-10-27 01:28:37 -0600

    Actualizo conexión para Render

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,14 +1,15 @@
 <?php
-$host = getenv('MYSQL_ADDON_HOST');
-$db   = getenv('MYSQL_ADDON_DB');
-$user = getenv('MYSQL_ADDON_USER');
-$pass = getenv('MYSQL_ADDON_PASSWORD');
-$port = getenv('MYSQL_ADDON_PORT') ?: 3306;
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
 
 try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("❌ Error de conexión: " . $e->getMessage());
+    die("Error de conexión: " . $e->getMessage());
 }
 ?>
+
