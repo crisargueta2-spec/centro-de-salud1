f70a2f99e293fa3157d1984783e3aab15b45b6ac
CristopherArg <TU_CORREO_DE_GITHUB>
2025-10-30 23:26:17 -0600

    Agrego includes/conexion.php para Render

---
+++ includes/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+
+try {
+    $conexion = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "❌ Error de conexión: " . $e->getMessage();
+    $conexion = null;
+}
+?>
