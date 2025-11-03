b4beafc8ca381a01889b11e3d77657ee397e70ef
crisargueta2-spec <crisargueta2@gmail.com>
2025-11-03 04:53:07 -0600

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -6,8 +6,9 @@ $password = getenv('DB_PASS');
 $dbname = getenv('DB_NAME');
 
 try {
-    $conexion = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Unificamos el nombre de la variable a $conn
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
