3263d41a213ad3357fae934d624e3dc9601b6405
crisargueta2-spec <crisargueta2@gmail.com>
2025-11-03 02:06:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/includes/session.php';
 require_once __DIR__ . '/includes/auth.php';
 require_once __DIR__ . '/includes/csrf.php';
 
-if (is_logged()) {
+if (is_logged_in()) {
   redirect_by_role(user()['rol']);
 }
 ?>
